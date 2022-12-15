cb83391379cee4b81ad7c6c222346ea5c4181a0b
skammar <shaikhammar12957@gmail.com>
2022-12-15 16:19:33 +0530

    M ADDING THIS JAVA FILE TO GITHUB

---
+++ Area.java
@@ -0,0 +1,26 @@
+class Area
+{
+  public static void main(String args[])
+ {
+   double a, r=6;
+   a=3.14*r*r;
+ 
+   System.out.println("Area of circle is:"+a);
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
